63567b61aee8fb418877cfe64de5b47bdf3365f4
Abraham Schneider <abe.schneider@gmail.com>
2014-07-15 07:16:09 -0400

    Fixed behavior of transform .. still needs work. Added ML, a mini-LISP like language that transforms into Julia expressions

---
+++ examples/ml.jl
@@ -0,0 +1,39 @@
+using EBNF
+using PEGParser
+
+@grammar mlgrammar begin
+  start = +(cell)
+  cell = space + list(lst | atom, space)
+  lst = lparen + +(cell) + rparen
+  atom = boolean | integer | string | symbol
+  boolean = "#t" | "#f"
+  integer = r"[1-9][0-9]*"
+  string = dquote + r"[^\"]*" + dquote
+  symbol = r"[^() ]+"
+  delimiter = lparen | rparen | space
+
+  dquote = "\""
+  lparen = "("
+  rparen = ")"
+  space = r"[ \n\r\t]*"
+end
+
+
+toexpr(node, cvalues, ::MatchRule{:default}) = cvalues
+toexpr(node, cvalues, ::MatchRule{:boolean}) = node.value == "#t" ? true : false
+toexpr(node, cvalues, ::MatchRule{:integer}) = parseint(node.value)
+toexpr(node, cvalues, ::MatchRule{:string}) = node.value[2:end-1]
+toexpr(node, cvalues, ::MatchRule{:symbol}) = symbol(node.value)
+
+toexpr(node, cvalues, ::MatchRule{:atom}) = cvalues
+toexpr(node, cvalues, ::MatchRule{:lst}) = Expr(:call, cvalues[1], cvalues[2], cvalues[3])
+toexpr(node, cvalues, ::MatchRule{:cell}) = cvalues
+
+
+data = "(+ 1 (- 5 6)) (* 3 4)"
+(ast, pos, error) = parse(mlgrammar, data)
+code = transform(toexpr, ast, ignore={:space, :dquote, :lparen, :rparen})
+
+for line in code
+  println(eval(line))
+end
