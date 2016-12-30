3fd614514e3d388457124beed9fc297daa7cb72f
vonDonnerstein <daschemiker@googlemail.com>
2016-12-30 17:46:46 +0100

    fixing 9e97458

--- src/parse.jl
+++ src/parse.jl
@@ -142,7 +142,7 @@ end
 function parse_newcachekey(grammar::Grammar, rule::Terminal, text::AbstractString, pos::Int, cache)
   local size::Int = length(rule.value)
 
-  if ismatch(Regex("\^$(rule.value)"),text[pos:end])
+  if startswith(text[pos:end],rule.value)
     node = make_node(rule, text[pos:pos+size-1], pos, pos+size, [])
     return (node, pos+size, nothing)
   end
