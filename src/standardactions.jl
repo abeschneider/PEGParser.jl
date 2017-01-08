ff22b3b402b1acb9989091ee68241a8fe552d467
vonDonnerstein <daschemiker@googlemail.com>
2017-01-08 16:55:49 +0100

    added more standardactions

--- src/standardactions.jl
+++ src/standardactions.jl
@@ -3,8 +3,19 @@ function no_action(rule, value, first, last, children)
 end
 
 function liftchild(rule, value, first, last, children)  # = default_action for OR-rules
-  if length(children) != 1
-    error("NYI")
+  if length(children) == 0
+    return nothing
+  elseif length(children) == 1
+    return children[1]
+  else
+    error("More than one child! rule: $rule; children: $children")
   end
-  return children[1]
+end
+
+function childrenarray(rule, value, first, last, children)
+  return children
+end
+
+function nodevalue(rule, value, first, last, children)
+  return value
 end
